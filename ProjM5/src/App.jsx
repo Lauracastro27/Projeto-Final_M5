232c778b8b7d8b6dabeaaea271749cf565da2029
Garduciz <94760419+Garduciz@users.noreply.github.com>
2022-06-04 16:06:19 -0300

    "api"

--- ProjM5/src/App.jsx
+++ ProjM5/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import Header from './components/header'
 import "./assents/index.css"
-
+import api from "./utils/axios"
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(api);
   return (
     <Header />
   )
