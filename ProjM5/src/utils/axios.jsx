8144d4839cf1110b5d6455e1a54c1dae366eb734
Garduciz <94760419+Garduciz@users.noreply.github.com>
2022-06-04 23:19:57 -0300

    adicionando a api

--- ProjM5/src/utils/axios.jsx
+++ ProjM5/src/utils/axios.jsx
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 import React from "react";
 
 const api = axios.create({
-  baseURL: "https://morning-scrubland-18153.herokuapp.com",
+  baseURL: "https://morning-scrubland-18153.herokuapp.com/",
 });
 
 export default api;
