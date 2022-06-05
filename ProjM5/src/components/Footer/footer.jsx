0747c58ee56df63b0c01a437e59f85f2921821ab
Luana-Developer <jrjrluana@gmail.com>
2022-06-04 22:35:21 -0300

    criando pag Sobre

--- ProjM5/src/components/Footer/footer.jsx
+++ ProjM5/src/components/Footer/footer.jsx
@@ -5,10 +5,10 @@ import style from "../Footer/footer.module.css";
 export default () => {
     return (
         <footer>
-            <div class={style.rowFlex}>
+            <div className={style.rowFlex}>
                 <div className={style.logo}>
                     <img src={Logo} className={style.logoimg} />
-                    <h1 class={style.colorText}>
+                    <h1 className={style.colorText}>
                         Projeto criado por GrupoI
                     </h1>
                 </div>
---
+++ ProjM5/src/components/Sobre/sobre.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "../Sobre/sobre.module.css";
+
+export default () => {
+    return (
+        <body>
+            <div>
+                <h1 className={style.corTitulo}>
+                    Sobre
+                </h1>
+                <p>
+                    Hello word
+                </p>
+            </div>
+        </body>
+    );
+};
